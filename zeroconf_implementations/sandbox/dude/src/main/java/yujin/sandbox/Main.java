c69e3fac20ae8610ccc4e460c0c3e6e66f7182e9
Daniel Stonier <d.stonier@gmail.com>
2011-11-07 21:41:58 +0900

    zeroconf: aha, finally osgi working.

--- zeroconf_implementations/sandbox/dude/src/main/java/yujin/sandbox/Main.java
+++ zeroconf_implementations/sandbox/dude/src/main/java/yujin/sandbox/Main.java
@@ -2,10 +2,10 @@ package yujin.sandbox;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-//import org.ros.address.InetAddressFactory;
-//import org.ros.node.Node;
-//import org.ros.node.NodeConfiguration;
-//import org.ros.node.DefaultNodeFactory;
+import org.ros.address.InetAddressFactory;
+import org.ros.node.Node;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.DefaultNodeFactory;
 
 public class Main {
 	/**
@@ -14,16 +14,16 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Dudes");
 		String masterUri = "http://localhost:11311";
-		System.out.println(System.getProperty("java.class.path"));
+		//System.out.println(System.getProperty("java.class.path"));
 		try {
 			URI uri = new URI(masterUri);
-//			String host = InetAddressFactory.newNonLoopback().getHostName();
-//			NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(host, uri);
-//			String defaultNodeName = "dude";
-//			
-//			DefaultNodeFactory factory = new DefaultNodeFactory();
-//			Node node = factory.newNode(defaultNodeName, nodeConfiguration);
-////			node.shutdown();
+			String host = InetAddressFactory.newNonLoopback().getHostName();
+			NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(host, uri);
+			String defaultNodeName = "dude";
+			
+			DefaultNodeFactory factory = new DefaultNodeFactory();
+			Node node = factory.newNode(defaultNodeName, nodeConfiguration);
+			node.shutdown();
 		} catch (URISyntaxException e) {
 			System.out.println("URI syntax exception");
 		}
